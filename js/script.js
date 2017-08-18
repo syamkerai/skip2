36efbdcd7475b315715fdeb37a418a0a5f3440c4
syamkerai <syamkerai@hotmail.com>
2017-08-18 21:32:39 +0300

    this is final edit, after js folder and linking it

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function pingPongClick() {
+  var number = document.getElementById("txt_pp").value;
+  var result = "";
+  for (var i = 1; i <= number; i++) {
+    if (i % 15 == 0) {
+      result += "<li>PingPong</li>";
+    } else if (i % 5 == 0) {
+      result += "<li>Pong</li>";
+    } else if (i % 3 == 0) {
+      result += "<li>Ping</li>";
+    } else {
+      result += "<li>" + i + "</li>";
+    }
+
+  }
+  document.getElementById("ans").innerHTML = result;
+}
